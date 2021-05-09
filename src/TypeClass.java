c108e35fc81c2c86c4016f31c1690c29e23d9073
Sangee <sangeetha8014@yahoo.com>
2021-05-09 06:07:13 +0000

    Typeclass

---
+++ src/TypeClass.java
@@ -0,0 +1,28 @@
+
+public class TypeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Implicit Type casting
+		
+		char a= 'a';
+		 
+		System.out.println("Value :" + a);
+		 
+		 int b = 'b';
+		 
+		 System.out.println("Value :" + b);
+		
+		// Explicit Type casting
+		 
+		 double num = 45.5;
+		 
+		 int num1 = (int) num;
+		 
+		 System.out.println("Value :" + num1);
+		 
+		 
+		 
+	}
+
+}
